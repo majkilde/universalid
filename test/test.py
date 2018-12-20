39798f241be5e18ee318fdcec412be351faa13cf
majkilde <jakob@majkilde.dk>
2018-12-20 11:25:40 +0100

    Refactor complete

--- test/test.py
+++ test/test.py
@@ -1,28 +1,29 @@
 import unittest
 from universalid import Unid
 import datetime
-
+from lib.settings import Settings
+from lib.encode import Encode
 
 class TestUniversalID(unittest.TestCase):
     max_time = datetime.datetime(2999, 12, 31, 23, 59, 59, 9998, tzinfo=None)
     min_time = datetime.datetime(1971, 1, 1, 0, 0, 0, 0, tzinfo=None)
 
     def test_int_to_string(self):
-        self.assertEqual(Unid._int_to_str(35, 3), '00z')
-        self.assertEqual(Unid._int_to_str(0, 3), '000')
-        self.assertEqual(Unid._int_to_str(999, 3), '0rr')
-        self.assertEqual(Unid._int_to_str(100, 3), '02s')
-        self.assertEqual(Unid._int_to_str(-100, 3), '02s')  # only absolute values.
-        self.assertEqual(Unid._int_to_str(99999, 3), 'zzz')  # out of range, return largest possible
+        self.assertEqual(Encode.int_to_str(35, 3), '00z')
+        self.assertEqual(Encode.int_to_str(0, 3), '000')
+        self.assertEqual(Encode.int_to_str(999, 3), '0rr')
+        self.assertEqual(Encode.int_to_str(100, 3), '02s')
+        self.assertEqual(Encode.int_to_str(-100, 3), '02s')  # only absolute values.
+        self.assertEqual(Encode.int_to_str(99999, 3), 'zzz')  # out of range, return largest possible
 
     def test_string_to_int(self):
-        self.assertEqual(Unid._str_to_int('z'), 35)
-        self.assertEqual(Unid._str_to_int('0000z'), 35)
-        self.assertEqual(Unid._str_to_int('zzz'), 46655)
+        self.assertEqual(Encode.str_to_int('z'), 35)
+        self.assertEqual(Encode.str_to_int('0000z'), 35)
+        self.assertEqual(Encode.str_to_int('zzz'), 46655)
 
     def test_encode_time(self):
-        self.assertEqual(Unid._encode_time(TestUniversalID.max_time), "exjul7z07pq")
-        self.assertEqual(Unid._encode_time(TestUniversalID.min_time), "00iruk00000")
+        self.assertEqual(Encode.encode_time(TestUniversalID.max_time), "exjul7z07pq")
+        self.assertEqual(Encode.encode_time(TestUniversalID.min_time), "00iruk00000")
 
     def test_timestamp(self):
         t_max = TestUniversalID.max_time.timestamp()
@@ -42,19 +43,19 @@ class TestUniversalID(unittest.TestCase):
         self.assertEqual(now, Unid.get_time(unid))
 
     def test_create(self):
-        self.assertEqual(len(Unid.create()), Unid.TOTAL_LENGTH)
-        self.assertEqual(len(Unid.create(prefix='__')), Unid.TOTAL_LENGTH)
-        self.assertEqual(Unid.create(prefix='__')[0:2], '__')
+        self.assertEqual(len(Unid.create()), Settings.TOTAL_LENGTH)
+        self.assertEqual(len(Unid.create(prefix='XX')), Settings.TOTAL_LENGTH)
+        self.assertEqual(Unid.create(prefix='XX')[0:2], 'XX')
         self.assertNotEqual(Unid.create(), Unid.create())
 
     def test_create_prefix(self):
         t = datetime.datetime.now()
         unid = Unid.create(t, prefix="ThisPrefixIsLongerThan20Characters")
-        self.assertEqual(len(unid), Unid.TOTAL_LENGTH)
+        self.assertEqual(len(unid), Settings.TOTAL_LENGTH)
         self.assertEqual(Unid.get_time(unid), t)
 
-        unid = Unid.create(prefix="Illegal Æ / ? ")
-        print(unid)
+        unid = Unid.create(prefix="Illegal Æ_./? Chars")
+        self.assertEqual(unid[:12], "ILLEGALCHARS")
 
 
 if __name__ == '__main__':
--- universalid.py
+++ universalid.py
@@ -4,6 +4,8 @@
 
 from datetime import datetime
 from secrets import randbelow
+from lib.settings import Settings
+from lib.encode import Encode
 
 
 # UNID format, default length = 32:
@@ -13,40 +15,25 @@ from secrets import randbelow
 # * Position 32: sequence (1 digig)
 
 class Unid:
-    BASE = 36  # (a-z + 10)
-
-    CUSTOM_LENGTH = 20
-    SEQUENCE_LENGTH = 1
-    DATETIME_LENGTH = 11  # total length of the datetime part, incl. the microseconds
-    TOTAL_LENGTH = CUSTOM_LENGTH + DATETIME_LENGTH + SEQUENCE_LENGTH  # total length of unid to generate
-
-    time_sequence = 0
-
-    # Max values with base 36
-    #   1 digit:    35
-    #   2 digits:   1.295
-    #   3 digits:   46.655
-    #   4 digits:   1.679.615
-    #   5 digits:   60.466.175
-    #   6 digits:   2.176.782.335
+    time_sequence = 0  # Add a sequence number to make the datetime part unique when Unid's is created simultaneously
 
     @staticmethod
     def create(time=None, prefix=None):
         time = time if time else datetime.utcnow()
-        prefix = prefix if prefix else ""
+        prefix = Unid._strip(prefix) if prefix else ""
 
-        # start with the optional prefix
-        unid = prefix[:Unid.CUSTOM_LENGTH]
+        # start with the optional prefix (limited to the max length)
+        unid = prefix[:Settings.CUSTOM_LENGTH]
 
         # add random chars
-        size = Unid.CUSTOM_LENGTH - len(prefix)
+        size = Settings.CUSTOM_LENGTH - len(prefix)
         unid += Unid._get_random(size)
 
         # add the datetime
-        unid += Unid._encode_time(time)
+        unid += Encode.encode_time(time)
 
         # add the sequence
-        unid += Unid._digit_to_char(Unid._inc_sequence())
+        unid += Encode.digit_to_char(Unid._inc_sequence())
 
         return unid.upper()
 
@@ -55,12 +42,12 @@ class Unid:
         # decode the datetime
         date_str = unid[-12:-5]
 
-        timestamp = Unid._str_to_int(date_str)
+        timestamp = Encode.str_to_int(date_str)
         date = datetime.fromtimestamp(timestamp)
 
         # decode the microseconds
         micro_str = unid[-5:-1]
-        microsecond = Unid._str_to_int(micro_str)
+        microsecond = Encode.str_to_int(micro_str)
 
         # return
         date = date.replace(microsecond=microsecond)
@@ -70,7 +57,7 @@ class Unid:
     def _inc_sequence():
         result = Unid.time_sequence
         Unid.time_sequence += 1
-        if Unid.time_sequence == Unid.BASE:
+        if Unid.time_sequence == Settings.BASE:
             Unid.time_sequence = 0
         return result
 
@@ -78,45 +65,13 @@ class Unid:
     def _get_random(length):
         s = ""
         while len(s) < length:
-            r = randbelow(Unid.BASE)
-            s += Unid._digit_to_char(r)
+            r = randbelow(Settings.BASE)
+            s += Encode.digit_to_char(r)
         return s
 
     @staticmethod
-    def _encode_time(time):
-        time_unid = Unid._int_to_str(int(time.timestamp()), 7)
-        micro_unid = Unid._int_to_str(time.microsecond, 4)
-
-        return time_unid + micro_unid
-
-    @staticmethod
-    def _int_to_str(number, width):
-        # convert to string
-        s = Unid._str_base(number)
-
-        # If number is to big, then return largest possible number
-        if len(s) > width:
-            return Unid._digit_to_char(Unid.BASE - 1) * width
-
-        # Return the number, padded with zeros
-        return ('0' * width + s)[-width:]
-
-    @staticmethod
-    def _str_to_int(s):
-        i = int(s, base=Unid.BASE)
-        return i
-
-    @staticmethod
-    def _digit_to_char(digit):
-        # encode a single digit to a char
-        if digit < 10:
-            return str(digit)
-        return chr(ord('a') + digit - 10)
-
-    @staticmethod
-    def _str_base(number):
-        # convert a number to an encoded string
-        (d, m) = divmod(abs(number), Unid.BASE)
-        if d > 0:
-            return Unid._str_base(d) + Unid._digit_to_char(m)
-        return Unid._digit_to_char(m)
+    def _strip(value):
+        result = ""
+        for c in value:
+            result += c if Encode.valid_char(c) else ''
+        return result
